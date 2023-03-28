f944183edac82bdcaa3a32a7c6892fb87c8f905c
BotDogs <ealexander1999@gmail.com>
2023-03-27 21:07:39 -0700

    combines with statements into a single one for simplicity

--- pathfinding&logging.py
+++ pathfinding&logging.py
@@ -114,15 +114,15 @@ if __name__ == '__main__':
     lg_stabf.add_variable('kalman.stateX', 'FP16')
     lg_stabf.add_variable('kalman.stateY', 'FP16')
     lg_stabf.add_variable('kalman.stateZ', 'FP16')
-    with SyncCrazyflie(urit, cf=Crazyflie(rw_cache='./cachet')) as tscf:
-        with SyncCrazyflie(urif, cf=Crazyflie(rw_cache='./cachef')) as fscf:
-            with PositionHlCommander(fscf, controller=PositionHlCommander.CONTROLLER_PID) as fpc:
-                with PositionHlCommander(tscf, controller=PositionHlCommander.CONTROLLER_PID) as tpc:
-                    simple_log_async_start(tscf, lg_stabt)
-                    simple_log_async_start(fscf, lg_stabf)
-                    time.sleep(1)
-                    # TODO: Change below code to FSM for diifferent conditions to show off features of our code
-                    # See nextsteps.txt for more info
-                    while True:
-                        pursue(fpc, lg_stabf.data, lg_stabt.data)
-                        time.sleep(0.01)
+    with(SyncCrazyflie(urif, cf=Crazyflie(rw_cache='./cachef')) as fscf,
+    SyncCrazyflie(urit, cf=Crazyflie(rw_cache='./cachet')) as tscf,
+    PositionHlCommander(tscf, controller=PositionHlCommander.CONTROLLER_PID) as tpc,
+    PositionHlCommander(fscf, controller=PositionHlCommander.CONTROLLER_PID) as fpc):
+        simple_log_async_start(tscf, lg_stabt)
+        simple_log_async_start(fscf, lg_stabf)
+        time.sleep(1)
+        # TODO: Change below code to FSM for diifferent conditions to show off features of our code
+        # See nextsteps.txt for more info
+        while True:
+            pursue(fpc, lg_stabf.data, lg_stabt.data)
+            time.sleep(0.01)
