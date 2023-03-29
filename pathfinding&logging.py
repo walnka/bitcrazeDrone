added acceleration prediction to the target prediction and corrected an error in the calculation of the yaw velocity

pathfinding&logging.py
@@ -43,6 +43,9 @@ logt.add_variable('stateEstimate.yaw', 'FP16')
 logt.add_variable('stateEstimate.vx', 'FP16')
 logt.add_variable('stateEstimate.vy', 'FP16')
 logt.add_variable('stateEstimate.vz', 'FP16')
+logt.add_variable('stateEstimate.ax', 'FP16')
+logt.add_variable('stateEstimate.ay', 'FP16')
+logt.add_variable('stateEstimate.az', 'FP16')
 logf = LogConfig(name='Stabilizer', period_in_ms=1000/freq)
 logf.add_variable('stateEstimate.x', 'FP16')
 logf.add_variable('stateEstimate.y', 'FP16')
@@ -51,6 +54,9 @@ logf.add_variable('stateEstimate.yaw', 'FP16')
 logf.add_variable('stateEstimate.vx', 'FP16')
 logf.add_variable('stateEstimate.vy', 'FP16')
 logf.add_variable('stateEstimate.vz', 'FP16')
+logf.add_variable('stateEstimate.ax', 'FP16')
+logf.add_variable('stateEstimate.ay', 'FP16')
+logf.add_variable('stateEstimate.az', 'FP16')
 #add other variables to be logged below
 
 # Only output errors from the logging framework
@@ -58,7 +64,14 @@ logging.basicConfig(level=logging.ERROR)
 
 def log_stab_callback(timestamp, data, logconf):
     temp=list(data.values())
-    temp.append((logconf.data[4]-temp[4])*freq)
+    if len(temp)==10:
+        temp2=[0,0,0]
+        for i in range(3):
+            temp2[2-i]=temp.pop(-1)
+    temp.append((temp[4]-logconf.data[4])*freq)
+    if len(temp)==10:
+        temp.append(temp2[:])
+        temp.append((temp[8]-logconf.data[8])*freq)
     logconf.data=temp
 
 def simple_log_async_start(scf, logconf):
@@ -95,17 +108,24 @@ def pursue(pc, fdata, tdata):
     # print(tdata[4:7])
     T_pos=np.array(tdata[0:3])
     T_yaw=np.array(tdata[3])/180*math.pi
-    T_vel=np.array(tdata[4:7])
-    T_yawv=np.array(tdata[7])/180*math.pi
-    F_pos=np.array(fdata[0:3])
-    # calculates the vector
-    diff=F_pos-T_pos
-    #calculates the yaw of the tracking drone should be the same as the above yaw within error
-    #T_yaw=math.atan2(2*(T_quat[3]*T_quat[0]+T_quat[1]*T_quat[2]),-(1-2*(T_quat[0]**2+T_quat[1]**2)))
-    #calculates the final drone position that is above and behind the tracking drone
     offset=tar_rad*np.array([-math.cos(T_yaw)*math.cos(hov_ang),-math.sin(T_yaw)*math.cos(hov_ang),math.sin(hov_ang)])
-    offsetvel=tar_rad*np.array([math.sin(T_yaw)*math.cos(hov_ang),-math.cos(T_yaw)*math.cos(hov_ang),0])*T_yawv
-    Final_pos=T_pos+offset+(T_vel+offsetvel)*pred
+    # calculates the final target position of the dorne
+    Final_pos=T_pos+offset
+    # if we are recording the velocity data it will use this to predict a final position of the drone
+    if len(tdata)>=8:
+        T_vel=np.array(tdata[4:7])
+        T_yawv=np.array(tdata[7])/180*math.pi
+        offsetvel=tar_rad*np.array([math.sin(T_yaw)*math.cos(hov_ang),-math.cos(T_yaw)*math.cos(hov_ang),0])*T_yawv
+        Final_pos=Final_pos+(T_vel+offsetvel)*pred
+    # if we are recording the acceleration data it will use this to predict the final position of the drone
+    if len(tdata)>=12:
+        T_acc=np.array(tdata[8:11])
+        T_yawa=np.array(tdata[11])/180*math.pi
+        offsetacc=tar_rad*(np.array([math.sin(T_yaw)*math.cos(hov_ang),-math.cos(T_yaw)*math.cos(hov_ang),0])*T_yawa+np.array([math.cos(T_yaw)*math.cos(hov_ang),math.sin(T_yaw)*math.cos(hov_ang),0])*T_yawv**2)
+        Final_pos=Final_pos+(T_acc+offsetacc)*pred**2
+    F_pos=np.array(fdata[0:3])
+    # calculates the vector between the 2 drones
+    diff=F_pos-T_pos
     #calculates the straight path vector between the flying drone and the target position
     diff_F=Final_pos-F_pos
     # finds the vector that has the shortest distance between the tracking drone and the straight line path of the flying drone to its target position
@@ -145,7 +165,7 @@ if __name__ == '__main__':
     #ensures that both drones are connected and are able to be flown
     with(SyncCrazyflie(urif, cf=Crazyflie(rw_cache='./cachef')) as fscf,
     SyncCrazyflie(urit, cf=Crazyflie(rw_cache='./cachet')) as tscf,
-    #PositionHlCommander(tscf, controller=PositionHlCommander.CONTROLLER_PID) as tpc,
+    PositionHlCommander(tscf, controller=PositionHlCommander.CONTROLLER_PID) as tpc,
     PositionHlCommander(fscf, controller=PositionHlCommander.CONTROLLER_PID) as fpc):
         # starts the logging
         simple_log_async_start(tscf, logt)